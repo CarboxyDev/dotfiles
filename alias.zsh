e5e867b5f233efb7c8a532c18a066d4e2ca6761b
Arman <37100541+CarboxyDev@users.noreply.github.com>
2022-12-15 03:06:18 +0530

    Remove rarely used aliases from alias.zsh

--- alias.zsh
+++ alias.zsh
@@ -14,29 +14,21 @@ alias ls='exa'
 alias top='btop'
 alias ping='ping -c 3'
 alias g++='g++ -std=c++17'
-alias size='du -h'
 alias python='python3.9'
 alias py='python3.9'
 alias j='javac Main.java && java Main'
 alias jc='javac'
-alias run='npm run dev'
 alias a='make a && ./a'
 alias finder='open .'
 alias apps='cd /Applications && open . && cd -'
 alias zshrc='vim ~/.zshrc'
-alias bashrc='vim ~/.bashrc'
-alias vimrc='vim ~/.vimrc'
 alias aliases='vim ~/Configs/zsh/alias.zsh'
-alias college='cd ~/College && open . && cd -'
-alias dev='cd ~/Dev'
-alias mygithub='cecho "CYAN" "My github: https://github.com/CarboxyDev/" && open -a safari "https://github.com/CarboxyDev/"'
 alias g='cd ~/Github'
 
 # Utilitity related aliases
 
-alias sha='shasum -a 256'
 alias diff='colordiff' # requires colordiff 
-alias myip='curl https://ipinfo.io/ip && echo "" && ipconfig getifaddr en0'
+alias ip='curl https://ipinfo.io/ip && echo "" && ipconfig getifaddr en0'
 
 # Fun and utterly useless aliases
 
