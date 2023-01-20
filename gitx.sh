44f2220e4f1fb52f31ece82f10daf502683c7a88
Arman <37100541+CarboxyDev@users.noreply.github.com>
2023-01-20 23:12:43 +0530

    Update and fix gitx.sh

--- gitx.sh
+++ gitx.sh
@@ -2,17 +2,20 @@
 
 function gitx() {
 	if [[ -z $1 ]]; then
-		echo "Usage: gitx -m [message]"
+		git add --all
+		git commit -m "Add latest changes"
+		git push
 	elif [[ "$1" == "-m" ]]; then
 		if [[ -n $2 ]]; then
-				git add .
-				git commit -m "$2"
-				git push
+			git add --all
+			git commit -m "$2"
+			git push
 		elif [[ -z $2 ]]; then
-				read -p "Proceed with automated commit message? (y/n): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
-				git add .
-				git commit -m "Changes to code [automated commit]"
-				git push
+			echo -n "Proceed with default commit message? (y/n): " 
+			read confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+			git add --all
+			git commit -m "Add latest changes"
+			git push
 				
 		fi
 	fi
