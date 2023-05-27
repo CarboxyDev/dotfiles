cd64d2f3c4035d6356b70bad2cb2b58a9ac0facf
Arman <37100541+CarboxyDev@users.noreply.github.com>
2023-05-27 21:38:52 +0530

    Update prompt.zsh

--- .prompt.zsh
+++ .prompt.zsh
@@ -3,7 +3,7 @@
 ### ZSH PROMPT
 
 
-# For the prompt shit, check out https://zsh.sourceforge.io/Doc/Release/Prompt-Expansion.html
+# For the prompt stuff, check out https://zsh.sourceforge.io/Doc/Release/Prompt-Expansion.html
 # For the colors, check out https://www.ditig.com/256-colors-cheat-sheet
 
 
@@ -19,13 +19,17 @@
 # 1 line prompt with small concise path and colored %/$ sign 
 #export PS1="%F{214}%m%F{015} %F{039}%1~ %F{007}%% %F{015}"
 
-# 1 line prompt with just compact path
-export PS1="%F{117}%1~ $ %F{015}%b"
+# 1 line prompt with just compact path + purple prompt
+#export PS1="%F{147}%1~ $ %F{255}%b"
 
+# 1 line prompt with just compact path + any color prompt
+#export PS1="%F{226}%1~ $ %F{255}%b"
+
+# 1 line prompt with just compact path + less colors
+export PS1="%F{247}%1~ $ %F{255}%b"
 
 # 1 line prompt with custom lambda marking and gold/purple colors
-# export PS1="%F{214}λ%F{015} %F{111}%1~ %B$ %F{015}%b"
-
+#export PS1="%F{214}λ%F{015} %F{111}%1~ %B$ %F{015}%b"
 
 # 1 line simple and very concise path
 #export PS1="%F{214}%F{015}%F{039}%1~ %F{007}%% %F{015}"
@@ -35,4 +39,3 @@ export PS1="%F{117}%1~ $ %F{015}%b"
 
 # 2 line prompt
 #export PS1="%F{214}%m%F{015} %F{039}%~ %F{015}"$'\n'"%F{214}->%F{015} "
-
