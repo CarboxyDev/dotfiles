38c3e3ac8c190a3065ec474608475cb8fb66f9db
CarboxyDev <carboxy17@gmail.com>
2026-02-18 17:09:36 +0530

    Revrert gitx to old behavior

--- gitx.zsh
+++ gitx.zsh
@@ -1,4 +1,8 @@
 gitx() {
-  local msg="${1:-Add latest changes}"
-  git add --all && git commit -m "$msg" && git push
+  if [[ "$1" == "-m" ]]; then
+    local msg="${2:-Add latest changes}"
+  else
+    local msg="${1:-Add latest changes}"
+  fi
+  git add --all && git commit -m "$msg"
 }
